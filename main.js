7df310d0c4f116182bf4aa79cf965d480de0f887
Lex Martinez <lex0316@gmail.com>
2018-05-13 11:18:54 -0500

    fixing window reactivation on osx

--- main.js
+++ main.js
@@ -12,9 +12,15 @@ if (process.env.ELECTRON_START_URL) {
 // To avoid being garbage collected
 let mainWindow
 
-app.on('ready', () => {
+const createWindow = () => {
 
-    let mainWindow = new BrowserWindow({width: 800, height: 600})
+    let mainWindow = new BrowserWindow({
+        height: 600,
+        useContentSize: true,
+        width: 900,
+        titleBarStyle: 'hidden',
+        maximizable: false
+      })
 
     const startUrl = process.env.ELECTRON_START_URL || url.format({
           pathname: path.join(__dirname, './build/index.html'),
@@ -24,28 +30,22 @@ app.on('ready', () => {
 
     mainWindow.loadURL(startUrl)
 
-    mainWindow.on('closed', function () {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        mainWindow = null
+    mainWindow.on('closed', () => {
+        mainWindow = undefined
     })
 
-})
+}
 
-// Quit when all windows are closed.
+app.on('ready', createWindow)
+
 app.on('window-all-closed', function () {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
         app.quit()
     }
 });
 
 app.on('activate', function () {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (mainWindow === null) {
+    if (mainWindow === undefined) {
         createWindow()
     }
 });
