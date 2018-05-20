e82c7eafe416b2e3b88b6185eae455c20448a5be
Lex Martinez <lex0316@gmail.com>
2018-05-20 11:04:21 -0500

    redux setup refactor - cleaning step

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
 import './style.css'
 export default ({ }: Header) => {
-    return (<div className={'navbar'}/>)
+  return (<div className={'navbar'}/>)
 }
--- src/typings.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'redux'
-declare module 'react-redux'
-declare module 'redux-thunk'
-declare module 'redux-logger'
-declare module 'react-router'
-declare module 'react-router-dom'
-declare module 'react-router-redux'
